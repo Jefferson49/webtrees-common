58c95fcc5b3de2017094d2c7dcfc1e1a7fd02b14
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-01-06 12:07:01 +0100

    Fixed case sensitive path issue, fixes #1

--- autoload.php
+++ autoload.php
@@ -25,9 +25,9 @@
  * 
  */
 
-$search = str_replace('/', DIRECTORY_SEPARATOR,'/vendor/jefferson49/webtrees-common');
-$dir    = str_replace($search,  '', strtolower(__DIR__));
-$loader = new Composer\Autoload\ClassLoader($dir .'/vendor');
+$search = str_replace('/', DIRECTORY_SEPARATOR,'/jefferson49/webtrees-common');
+$dir    = str_replace($search,  '', __DIR__);
+$loader = new Composer\Autoload\ClassLoader($dir);
 
 try {
     $autoload_common_library_version = Composer\InstalledVersions::getVersion('jefferson49/webtrees-common');
@@ -36,14 +36,14 @@ catch (\OutOfBoundsException $e) {
     $autoload_common_library_version = '';
 }
 
-$local_composer_versions = require $dir . '/vendor/composer/installed.php';
+$local_composer_versions = require $dir . '/composer/installed.php';
 $local_common_library_version = $local_composer_versions['versions']['jefferson49/webtrees-common']['version'];
 
 //If the found library is later than the current autoload version, prepend the found library to autoload
 //This ensures that always the latest library version is autoloaded
 if (version_compare($local_common_library_version, $autoload_common_library_version, '>')) {
-    $loader->addPsr4('Jefferson49\\Webtrees\\Helpers\\', $dir . '/vendor/jefferson49/webtrees-common/Helpers');
-    $loader->addPsr4('Jefferson49\\Webtrees\\Internationalization\\', $dir. '/vendor/jefferson49/webtrees-common/Internationalization');    
-    $loader->addPsr4('Jefferson49\\Webtrees\\Log\\', $dir . '/vendor/jefferson49/webtrees-common/Log');
-    $loader->register(true);    
+    $loader->addPsr4('Jefferson49\\Webtrees\\Helpers\\', __DIR__ . '/Helpers');
+    $loader->addPsr4('Jefferson49\\Webtrees\\Internationalization\\', __DIR__ . '/Internationalization');
+    $loader->addPsr4('Jefferson49\\Webtrees\\Log\\',  __DIR__ . '/Log');
+    $loader->register(true);
 }
